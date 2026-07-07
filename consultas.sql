303f529f4456fa6fa49851adbc5abc8f6be5bf98
sanderson.mariano <sandersonmariano326@gmail.com>
2026-07-07 01:47:08 -0300

    Update consultas.sql

--- consultas.sql
+++ consultas.sql
@@ -1,1 +1,11 @@
+-- Mostrar todos os livros
+SELECT * FROM livros;
 
+-- Mostrar todos os autores
+SELECT * FROM autores;
+
+-- Mostrar todos os usuários
+SELECT * FROM usuarios;
+
+-- Mostrar todos os empréstimos
+SELECT * FROM emprestimos;
